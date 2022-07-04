d44f9e70dcba95a7d62f98fde63b8492c696b6dd
DoHeon16 <58592231+DoHeon16@users.noreply.github.com>
2022-07-04 16:32:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/specialmps/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.specialmps
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

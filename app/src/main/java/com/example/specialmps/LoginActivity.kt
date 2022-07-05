8836a8adf2969e27828c10bdc7afcb27a8f76b07
DoHeon16 <58592231+DoHeon16@users.noreply.github.com>
2022-07-05 13:54:04 +0900

    login0.2

--- app/src/main/java/com/example/specialmps/LoginActivity.kt
+++ app/src/main/java/com/example/specialmps/LoginActivity.kt
@@ -2,11 +2,36 @@ package com.example.specialmps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
+
+    //val mDatabase=FirebaseDatabase.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun checkID(id:String){
+        //val database=mDatabase.get
+    }
+
+    fun checkPW(){
+
+    }
+
+    fun btnClick(){
+        signup.setOnClickListener {
+
+        }
+
+        login.setOnClickListener {
+            val id=id.text.toString()
+            val pw=password.text.toString()
+
+            checkID(id)
+        }
     }
 }

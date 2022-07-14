ea275d4382a7395ff984c84e2117bb4c0649473f
DoHeon16 <58592231+DoHeon16@users.noreply.github.com>
2022-07-14 22:31:14 +0900

    login0.3
    modified login activity

--- app/src/main/java/com/example/specialmps/LoginActivity.kt
+++ app/src/main/java/com/example/specialmps/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.specialmps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -17,16 +20,30 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnClick()
     }
 
-    fun checkID(id:String){
+    fun checkAccount(id:String, pw:String){
         val database=mDatabase.getReference("Users")
         database.addValueEventListener(object :ValueEventListener{
 
             override fun onDataChange(snapshot: DataSnapshot) {
                 for(shot in snapshot.children){
                     if(shot.key.toString()==id){
-                        
+                        val user=shot.getValue(UserInfo::class.java)
+                        if(user!=null){
+                            if(user.pw==pw){
+                                Toast.makeText(this@LoginActivity,user.name+"님이 로그인하셨습니다.",Toast.LENGTH_SHORT).show()
+                                val i=Intent(this@LoginActivity,MainActivity::class.java)
+                                i.putExtra("userID",id)
+                                startActivity(i)
+                                finish()
+                            }
+                        }else{
+                            Toast.makeText(this@LoginActivity,"없는 계정이거나 비밀번호가 맞지 않습니다.",Toast.LENGTH_SHORT).show()
+                        }
+                    }else{
+                        Toast.makeText(this@LoginActivity,"없는 계정이거나 비밀번호가 맞지 않습니다.",Toast.LENGTH_SHORT).show()
                     }
                 }
             }
@@ -38,20 +55,24 @@ class LoginActivity : AppCompatActivity() {
         })
     }
 
-    fun checkPW(){
-
-    }
-
     fun btnClick(){
         signup.setOnClickListener {
-
+            //회원가입 창
+            val i=Intent(this,Signup::class.java)
+            startActivity(i)
+            finish()
         }
 
         login.setOnClickListener {
-            val id=id.text.toString()
-            val pw=password.text.toString()
+            val _id=id.text.toString()
+            val _pw=password.text.toString()
 
-            checkID(id)
+            checkAccount(_id,_pw)
+            val hand=Handler()
+            hand.postDelayed({
+                id.setText("")
+                password.setText("")
+            },2000)
         }
     }
 }
--- app/src/main/java/com/example/specialmps/UserInfo.kt
+++ app/src/main/java/com/example/specialmps/UserInfo.kt
@@ -1,3 +1,6 @@
 package com.example.specialmps
 
 data class UserInfo(var name:String, var id:String, var pw:String, var sex:String, var birth:String)
+{
+    constructor():this("noinfo","noinfo","noinfo","noinfo","noinfo")
+}

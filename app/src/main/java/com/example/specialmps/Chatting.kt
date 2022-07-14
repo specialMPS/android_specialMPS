d9b798ad89608a0de509231e135988fc5f2d6805
DoHeon16 <58592231+DoHeon16@users.noreply.github.com>
2022-07-14 23:24:28 +0900

    version upgrade
    upgrade
    targetsdkversion 30
    gradle version 6.1.1
    android gradle plugin version 4.0.0

---
+++ app/src/main/java/com/example/specialmps/Chatting.kt
@@ -0,0 +1,11 @@
+package com.example.specialmps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Chatting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chatting)
+    }
+}

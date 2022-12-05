cb076155e71501150c1207c086433a65848bff1f
YuBeen-Park <park_ubin@naver.com>
2022-12-05 16:02:23 +0900

    [chore] url

--- app/src/main/java/com/example/specialmps/ChattingActivity.kt
+++ app/src/main/java/com/example/specialmps/ChattingActivity.kt
@@ -41,7 +41,7 @@ class ChattingActivity : AppCompatActivity() {
     var userid: String = ""
     val mDatabase = FirebaseDatabase.getInstance()
     var chat_start_time: String = ""
-    val serverURL = "http://192.168.198.210:8080/chat?s="
+    val serverURL = "http://172.30.1.16:8080/chat?s="
     val BASE_URL = "http://172.20.10.12:8080/emotion?s="
     var userChat: String = " "
     var emotionScore: EmotionInfo = EmotionInfo()
@@ -255,7 +255,7 @@ class ChattingActivity : AppCompatActivity() {
                     .readTimeout(3, TimeUnit.MINUTES)
                     .writeTimeout(10, TimeUnit.SECONDS)
                     .build()
-                val BASE_URL = "http://192.168.198.210:8080/emotion?s=".plus(userChat)
+                val BASE_URL = "http://172.30.1.16:8080/emotion?s=".plus(userChat)
                 var req = Request.Builder().url(BASE_URL).build()
                 client.newCall(req).enqueue(object : Callback {
                     override fun onFailure(call: Call, e: IOException) {
